6cc26b8c2bd2f69fb3dc04b9bccdc7b8f506e619
Nick <nickask12@gmail.com>
2022-02-09 17:30:48 +0100

    Initial Commit

---
+++ Busamigo/BusamigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusamigoApp.swift
+//  Busamigo
+//
+//  Created by Nick Askari on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BusamigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
