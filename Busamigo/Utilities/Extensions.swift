991922c1b210f78624d749048dc953d8a51e8646
Nick <nickask12@gmail.com>
2022-02-22 00:47:33 +0100

    idk

---
+++ Busamigo/Utilities/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  Busamigo
+//
+//  Created by Nick Askari on 18/02/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
